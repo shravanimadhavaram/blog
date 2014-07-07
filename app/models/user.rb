3d8a76b2454ebf30815c33b406615a146a3f6464
shravani <shravanimadhavaram>
2014-07-07 17:01:54 +0530

    initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+class Person
+  include ActiveModel::Validations
+
+  attr_accessor :Email
+end
+User.attribute_method?(:Email) # => true
+User.attribute_method?(:Password)  # => false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '319562cc76ddd81f133a8dd317cb0e4a50d3e13a36f80a2e6386c0a4204ad9b099b9747fe18e64a6972cf8bb8d2b12b74c38b965932eff738933c45d5fab1bbe'
